9622a5fe963dfda249df91f8187313a7f23adc38
Nils Meissner <nils_olaf_meissner@web.de>
2025-04-09 17:47:34 +0100

    Add another test

--- towers-view/libs/growth-job/src/lib/growth-job/growth-job.component.spec.ts
+++ towers-view/libs/growth-job/src/lib/growth-job/growth-job.component.spec.ts
@@ -1,9 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { GrowthJobComponent } from './growth-job.component';
+import { GrowthJob } from 'apps/towers-view/src/app/simulation/growth-job';
 
 describe('GrowthJobComponent', () => {
   let component: GrowthJobComponent;
   let fixture: ComponentFixture<GrowthJobComponent>;
+  let expectedGrowthJob: GrowthJob;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -12,8 +14,16 @@ describe('GrowthJobComponent', () => {
 
     fixture = TestBed.createComponent(GrowthJobComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture.autoDetectChanges();
+
+    // mock the growth job supplied by the parent component
+    expectedGrowthJob = new GrowthJob('Fruit', 50);
+      
+    // simulate the parent setting the input property with that hero
+    fixture.componentRef.setInput('growthJob', expectedGrowthJob);
+    // wait for initial data binding
+    await fixture.whenStable();
+});
 
   it('should create', () => {
     expect(component).toBeTruthy();
@@ -21,6 +31,14 @@ describe('GrowthJobComponent', () => {
 
   it('should have a title', () => {
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.title')?.textContent).toContain('GrowthJob: ');
+    expect(compiled.querySelector('.title')?.textContent).toContain('GrowthJob: ' + expectedGrowthJob.name);
+  });
+
+  it('it should have progress bar', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const progressBarElem = compiled.querySelector('.progress-bar');
+    expect(progressBarElem).toBeTruthy();
+    if (progressBarElem)
+      expect(getComputedStyle(progressBarElem).width).toEqual(expectedGrowthJob.progressPercentage + '%');
   });
 });
